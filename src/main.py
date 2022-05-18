d26ff9364c5f868b7f936aae259c71d760270254
Blackwood <blackwoodj3311@gmail.com>
2022-05-18 14:18:18 -0500

    tiny adjustment dw

--- src/main.py
+++ src/main.py
@@ -1,10 +1,5 @@
 import sys
-from gui import MainWindow
-
-from PyQt5.QtWidgets import *
-from PyQt5 import QtCore, QtGui
-from PyQt5.QtGui import *
-from PyQt5.QtCore import *
+from gui import *
 
 def main():
     app = QApplication(sys.argv)
